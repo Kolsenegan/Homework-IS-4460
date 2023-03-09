e00b90866bf716c6e4ade6811939b3b722ded285
Kolsen Egan <125928092+Kolsenegan@users.noreply.github.com>
2023-03-10 00:56:26 +0100

    Add files via upload

---
+++ HW 4/user-list.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+	<title>User List</title>
+	
+	</head>
+	
+	<body>
+	<!-- User List -->
+	<a href="user-list.php">User List</a>
+	<a href="user-add.php">Add User</a>
+	<a href="logout.php">Log Out</a>
+	<br>
+	<H1>Users</H1>
+	
+
+	</body>
+</html>
+
+<?php
+
+$page_roles = array('admin', 'user');
+
+require_once  'login.php';
+require_once  'checksession.php';
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+$query = "SELECT * FROM users";
+
+$result = $conn->query($query); 
+if(!$result) die($conn->error);
+
+$rows = $result->num_rows;
+
+for($j=0; $j<$rows; $j++)
+{
+	//$result->data_seek($j); 
+	$row = $result->fetch_array(MYSQLI_ASSOC); 
+
+echo <<<_END
+	<pre>
+	Forename: <a href='user-details.php?id=$row[id]'>$row[forename]</a>
+	
+	</pre>
+	
+	
+	
+_END;
+
+}
+
+$conn->close();
+
+
+
+?>
